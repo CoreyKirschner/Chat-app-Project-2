3f57fc09a3216bf043b7a90931cd92a044acd957
NguyenSang23 <nguyensang0323@gmail.com>
2023-02-08 17:05:42 -0800

    fixed handlebars for login, signup, profile

--- public/src/signUp.js
+++ public/src/signUp.js
@@ -1,66 +1,62 @@
-const form = document.querySelector('#form');
-const username = document.querySelector('#username');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
-const password2 = document.querySelector('#password2');
+const form = document.querySelector("#form");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const password2 = document.querySelector("#password2");
 
-
-form.addEventListener('submit' ,(event) => {
-    event.preventDefault()
-    checkInput()
-    fetch("" , {
-        method:"post" , 
-        body: {
-        email:document.getElementById("email").value , 
-        password:document.getElementById("password").value ,
-        username:document.getElementById("username").value ,
-        password2:document.getElementById("password2").value
-        }
-    })
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(text) {
-        console.log()
-    })
-    
-})
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const response = await fetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      email: document.getElementById("email").value,
+      password: document.getElementById("password").value,
+      username: document.getElementById("username").value,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+  if (response.ok) {
+    document.location.replace("/profile");
+  } else {
+    alert("sign up failed");
+  }
+});
 
 const checkInput = () => {
-    const usernameValue = username.value.trim()
-    const emailValue = email.value.trim()
-    const passwordValue = password.value.trim()
-    const password2Value = password2.value.trim()
-    if(usernameValue === '') {
-        setError(username , 'please enter the username')
-    } else {
-        setSucces(username)
-    }
-    if(emailValue === '') {
-        setError(email , 'please enter the email')
-    } else {
-        setSucces(email)
-    }
-    if(passwordValue === '') {
-        setError(password , 'please enter the password')
-    } else {
-        setSucces(password)
-    }
-    if(password2Value === '') {
-        setError(password2 , 'please re-enter the password')
-    } else if(passwordValue !== password2Value) {
-        setError(password2 , 'it is the wrong password')
-    } else {
-        setSucces(password2)
-    }
-}
-const setError = (input , message) => {
-    const formControl = input.parentElement
-    const small = formControl.querySelector('small')
-    formControl.className = 'form-control error'
-    small.innerText = message
-}
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+  if (usernameValue === "") {
+    setError(username, "please enter the username");
+  } else {
+    setSucces(username);
+  }
+  if (emailValue === "") {
+    setError(email, "please enter the email");
+  } else {
+    setSucces(email);
+  }
+  if (passwordValue === "") {
+    setError(password, "please enter the password");
+  } else {
+    setSucces(password);
+  }
+  if (password2Value === "") {
+    setError(password2, "please re-enter the password");
+  } else if (passwordValue !== password2Value) {
+    setError(password2, "it is the wrong password");
+  } else {
+    setSucces(password2);
+  }
+};
+const setError = (input, message) => {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+  formControl.className = "form-control error";
+  small.innerText = message;
+};
 const setSucces = (input) => {
-    const formControl = input.parentElement
-    formControl.className = 'form-control succes'
-}
+  const formControl = input.parentElement;
+  formControl.className = "form-control succes";
+};
