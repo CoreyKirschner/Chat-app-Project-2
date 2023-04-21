e8a15b78df6d0484f91715fd01811f84e8dd2a9f
CoreyKirschner <114973010+CoreyKirschner@users.noreply.github.com>
2023-04-20 23:04:08 -0700

    Update homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,43 +3,55 @@ const { User, Chat, Response } = require("../models");
 const withAuth = require("../utils/auth.js");
 
 router.get("/", async (req, res) => {
-  res.render("homepage");
+  try {
+    res.render("homepage");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get("/login", async (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
+  try {
+    if (req.session.loggedIn) {
+      res.redirect("/");
+      return;
+    }
+    res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
   }
-  res.render("login");
 });
 
 router.get("/chat", async (req, res) => {
-  res.render("chat");
+  try {
+    res.render("chat");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get("/profile", withAuth, async (req, res) => {
   try {
-    console.log(req.session.userId);
     const chatData = await Chat.findAll({
       where: { userId: req.session.userId },
     });
-    console.log(chatData);
     const chat = chatData.map((chat) => chat.get({ plain: true }));
-    console.log("chat", chat);
     res.render("profile", { chat });
   } catch (err) {
-    res.status(400).json(err.message);
+    res.status(500).json(err);
   }
 });
 
 router.get("/signup", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
+  try {
+    if (req.session.loggedIn) {
+      res.redirect("/");
+      return;
+    }
+    res.render("signup");
+  } catch (err) {
+    res.status(500).json(err);
   }
-
-  res.render("signup");
 });
 
 module.exports = router;
