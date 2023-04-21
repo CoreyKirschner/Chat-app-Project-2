5a13d6e903134f131e2777a704dd36f52b8b8210
CoreyKirschner <114973010+CoreyKirschner@users.noreply.github.com>
2023-04-20 21:03:57 -0700

    Update index.js
    commenting out response routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const chatRoutes = require("./chatRoutes");
 const userRoutes = require("./userRoutes");
-const responseRoutes = require("./responseRoutes");
+//const responseRoutes = require("./responseRoutes");
 
 router.use("/users", userRoutes);
 router.use("/chats", chatRoutes);
-router.use("/response", responseRoutes);
+//router.use("/response", responseRoutes);
 
 module.exports = router;
