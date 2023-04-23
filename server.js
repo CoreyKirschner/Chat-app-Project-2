99d3921bbf4eb00d939d3ad16fc4a82612f940b7
CoreyKirschner <114973010+CoreyKirschner@users.noreply.github.com>
2023-04-23 01:34:17 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const helpers = require("./utils/helpers");
+
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+// Import sequelize connection
 const sequelize = require("./config/config");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+// Set up session middleware
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sess = {
   secret: process.env.SESS_SECRET,
   cookie: {
@@ -20,21 +23,28 @@ const sess = {
     db: sequelize,
   }),
 };
-
 app.use(session(sess));
 
+// Set up handlebars engine and views
 const hbs = exphbs.create({ helpers });
-
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
 
+// Middleware to parse JSON and urlencoded request bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require("./controllers/"));
+// Set up routes
+const routes = require("./controllers");
+app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
+// Start the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
 });
