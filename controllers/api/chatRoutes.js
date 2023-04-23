1f67360f6cf4020e609d14a06b1266825079144f
CoreyKirschner <114973010+CoreyKirschner@users.noreply.github.com>
2023-04-23 01:33:33 -0700

    Update chatRoutes.js

--- controllers/api/chatRoutes.js
+++ controllers/api/chatRoutes.js
@@ -18,19 +18,19 @@ router.post("/", withAuth, async (req, res) => {
 
 router.delete("/:id", withAuth, async (req, res) => {
   try {
-    const projectData = await Chat.destroy({
+    const chatData = await Chat.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
+        user_id: req.session.userId,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: "No project found with this id!" });
+    if (!chatData) {
+      res.status(404).json({ message: "No chat found with this id!" });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(chatData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -56,3 +56,4 @@ router.put("/:id", withAuth, async (req, res) => {
 });
 
 module.exports = router;
+
